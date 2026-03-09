31c0e5a9e0df875e8284a48e669c1e2d0920d334
Amin Aziz Sudjud <azizsudjud@gmail.com>
2026-03-10 01:07:53 +0700

    Menambahkan Tabs pada create/edit halaman Product (Jobsheet 9)

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -7,6 +7,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 
 class ProductInfolist
 {
@@ -15,6 +17,56 @@ class ProductInfolist
         return $schema
             ->components([
                 //
+                Tabs::make('Product Tabs')
+                    ->tabs([
+                        Tabs\Tab::make('Product Info')
+                            ->icon('heroicon-o-academic-cap')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+                                TextEntry::make('sku')
+                                    ->label('SKU')
+                                    ->badge()
+                                    ->color('success'),
+                                TextEntry::make('description')
+                                    ->label('Description'),
+                                TextEntry::make('created_at')
+                                    ->label('Created At')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ]),
+                        Tab::make('Pricing & Stock')
+                        ->icon('heroicon-o-currency-dollar')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Price')
+                                    ->icon('heroicon-o-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Stock')
+                                    ->badge()
+                                    ->color(fn ($state) => $state > 200 ? 'success' : 'danger')
+                            ]),
+                        Tabs\Tab::make('Media & Status')
+                            ->icon('heroicon-o-photo')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                IconEntry::make('is_visible')
+                                    ->label('Active')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Featured')
+                                    ->boolean(),
+                            ]),
+                    ])
+                    ->columnSpanFull()
+                    ->vertical(),
+
                 Section::make('Product Info')
                     ->schema([
                         TextEntry::make('name')
@@ -54,7 +106,7 @@ class ProductInfolist
                             ->label('Product Price')
                             ->weight('bold')
                             ->color('primary')
-                            ->formatStateUsing(fn ($state) => 'Rp ' . number_format($state, 2))
+                            ->formatStateUsing(fn($state) => 'Rp ' . number_format($state, 2))
                             ->icon('heroicon-o-currency-dollar'),
                         TextEntry::make('stock')
                             ->label('Product Stock')
@@ -68,7 +120,6 @@ class ProductInfolist
                             ->label('Is Featured')
                             ->boolean(),
                     ]),
-
             ]);
     }
 }
